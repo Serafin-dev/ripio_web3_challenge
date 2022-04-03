dd38d9fe9c2d924898e3e580ece09335870dfd7d
Serafin-dev <82891246+Serafin-dev@users.noreply.github.com>
2022-04-03 11:45:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 // call a callback function as second parameter TO KNOW WHEN THE API IS READY (OPTIONAL)
-app.listen(PORT, () => console.log(`It is alive on http://localhost:${PORT}`)) // conectas la web al servidor
+app.listen(PORT, () => console.log(`It is alive on http://localhost:${PORT}`)); // conectas la web al servidor
 // JsonRPC provider
 const provider = new ethers.providers.JsonRpcProvider(process.env.PROVIDER);
 // contract interface
-const abi = [{"constant":false,"inputs":[{"name":"_name","type":"string"}],"name":"createProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"productToOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_productId","type":"uint256"},{"name":"_newOwner","type":"address"}],"name":"delegateProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_productId","type":"uint256"}],"name":"acceptProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"products","outputs":[{"name":"name","type":"string"},{"name":"status","type":"uint8"},{"name":"owner","type":"address"},{"name":"newOwner","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"size","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"productId","type":"uint256"},{"indexed":false,"name":"name","type":"string"}],"name":"NewProduct","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"productId","type":"uint256"},{"indexed":false,"name":"newOwner","type":"address"},{"indexed":false,"name":"status","type":"uint8"}],"name":"DelegateProduct","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"productId","type":"uint256"},{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"status","type":"uint8"}],"name":"AcceptProduct","type":"event"}]
+const abi = [{"constant":false,"inputs":[{"name":"_name","type":"string"}],"name":"createProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"productToOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_productId","type":"uint256"},{"name":"_newOwner","type":"address"}],"name":"delegateProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_productId","type":"uint256"}],"name":"acceptProduct","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"products","outputs":[{"name":"name","type":"string"},{"name":"status","type":"uint8"},{"name":"owner","type":"address"},{"name":"newOwner","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"size","outputs":[{"name":"count","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"productId","type":"uint256"},{"indexed":false,"name":"name","type":"string"}],"name":"NewProduct","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"productId","type":"uint256"},{"indexed":false,"name":"newOwner","type":"address"},{"indexed":false,"name":"status","type":"uint8"}],"name":"DelegateProduct","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"productId","type":"uint256"},{"indexed":false,"name":"name","type":"string"},{"indexed":false,"name":"status","type":"uint8"}],"name":"AcceptProduct","type":"event"}];
 // contract
-const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, abi, provider)
+const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, abi, provider);
 
 // All Products 
 // como segundo parametro se le pasa una CALLBACK function, que SE EJECUTA CUANDO UN USUARIO ingresa a esta ruta.
@@ -31,23 +31,23 @@ app.get('/api/products', async (req, res) => {
     //response 
     res.status(200).send({
         all_products    
-    })
+    });
 });
 
 // Create product
 app.post('/api/create_product/:name', async (req, res) => {
     
     // Request 
-    const { name } = req.params;
+    const { name } = req.params
 
     // Ckeck params
     if (!name){
         res.status(418).send({
             message : "You need to give the product a name! "
-        })
-    }
+        });
+    };
     // Backend Wallet
-    const backend_Wallet = new ethers.Wallet(process.env.PK, provider) 
+    const backend_Wallet = new ethers.Wallet(process.env.PK, provider);
 
     //Transaction
     const tx = await products.createNewProduct(contract, backend_Wallet, name);
@@ -73,8 +73,8 @@ app.post('/api/delegate_product/:id', async (req, res) => {
     if(!id || !from || !to){
         res.status(418).send({
             message : "Product Id, public key and private key are needed"
-        })
-    }
+        });
+    };
     // Owner Wallet
     const Wallet = new ethers.Wallet(from, provider) // sender Wallet
     
@@ -83,7 +83,7 @@ app.post('/api/delegate_product/:id', async (req, res) => {
     res.send({
         message : `Product succesfully delegated to ${to}`,
         tx 
-    })
+    });
 
 });
 
@@ -91,15 +91,15 @@ app.post('/api/delegate_product/:id', async (req, res) => {
 // {"pk":"9ec233b6619add55c73f0445708ab0ead286d6ddad95a9cb4b220b0a77722ad4"}
 app.post('/api/accept_product/:id', async (req, res) => {
     // Request
-    const { id } = req.params;//product id
-    const pk  = req.body.pk; // newOwner private key
+    const { id } = req.params //product id
+    const pk  = req.body.pk // newOwner private key
 
     // Check params
     if (!pk || !id){
         res.status(418).send({
             message : " ¡A private Key and an id are needed! "
-        })
-    }
+        });
+    };
     // Owner Wallet
     const Wallet = new ethers.Wallet(pk, provider) // sender Wallet
 
@@ -110,7 +110,7 @@ app.post('/api/accept_product/:id', async (req, res) => {
     res.send({
         message : "Product accepted",
         tx
-    })
+    });
 });
 
 // See Products from owner
@@ -121,13 +121,13 @@ app.get('/api/owner_products/:owner', async (req, res) => {
     if (!owner){
         res.status('418').send({
             message: "A public Key is needed"
-        })
-    }
+        });
+    };
     // get products from acc
-    const products_from_owner = await products.getProductsFromOwner(contract, owner)
+    const products_from_owner = await products.getProductsFromOwner(contract, owner);
     
     //response 
     res.status('200').send({
         products_from_owner
-    })
+    });
 });
